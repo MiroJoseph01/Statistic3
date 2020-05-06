99a729157fa37d35ba7e5ddd35fa11b6813fb152
MiroJoseph01 <49228848+MiroJoseph01@users.noreply.github.com>
2020-05-06 11:37:23 +0300

    initial commit

---
+++ src/main/java/Solution.java
@@ -0,0 +1,15 @@
+public class Solution {
+    private double [] Xi;
+    private double [] Yi;
+    private int length;
+
+    public Solution(double[] Xi, double[] Yi){
+        this.Xi=Xi;
+        this.Yi=Yi;
+        length=Xi.length;
+    }
+
+    public void estimationOfCorrelationCoefficient(){
+
+    }
+}
