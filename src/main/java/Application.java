a42eec924e5a3a7322ec9bcafdfd5ab1be950e75
timstuf <nastushagrishaeva@gmail.com>
2020-05-06 12:55:06 +0300

    linear regression done

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -2,6 +2,9 @@
 
 public class Application {
     public static void main(String[] args) {
-
+        double[] x = {6.7, 6.9, 7.2, 7.3, 8.4, 8.8, 9.1, 9.8, 10.6, 10.7, 11.1, 11.8, 12.1, 12.4};
+        double[] y = {2.8, 2.2, 3, 3.5, 3.2, 3.7, 4, 4.8, 6, 5.4, 5.2, 5.4, 6, 9};
+        Solution solution = new Solution(x, y);
+        solution.regressionLine();
     }
 }
--- src/main/java/Solution.java
+++ src/main/java/Solution.java
@@ -5,7 +5,7 @@ public class Solution {
     private double xExpectation;
     private double yExpectation;
     private double selectionCorrelationsAssignment; //центральный момент 11
-    private double xDispersion;
+    private double xDispersion; //неисправленная!!
 
     public Solution(double[] Xi, double[] Yi) {
         this.Xi = Xi;
@@ -19,7 +19,10 @@ public class Solution {
 
     public void regressionLine() {
         double bYX = selectionCorrelationsAssignment / Math.sqrt(xDispersion);
-        System.out.printf("b yx = %.3f/%.3f = %.3f", selectionCorrelationsAssignment, Math.sqrt(xDispersion), bYX);
-
+        System.out.printf("b_yx = %.3f/%.3f = %.3f\n", selectionCorrelationsAssignment, Math.sqrt(xDispersion), bYX);
+        System.out.println("y_x = b_yx * x + (<y> -  b_yx * <x>)");
+        System.out.printf("y_x = %.3f * x + (%.3f -  %.3f * %.3f)", bYX, yExpectation, bYX, xExpectation);
+        double b = yExpectation - bYX * xExpectation;
+        System.out.printf("y_x = %.3f * x + %.3f\n", bYX, b);
     }
 }
