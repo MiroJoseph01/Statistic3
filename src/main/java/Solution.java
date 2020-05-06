ab9006737249376cd092c7d9b810c3eab4d4a975
timstuf <nastushagrishaeva@gmail.com>
2020-05-06 11:56:44 +0300

    added some fields

--- src/main/java/Solution.java
+++ src/main/java/Solution.java
@@ -1,15 +1,25 @@
 public class Solution {
-    private double [] Xi;
-    private double [] Yi;
+    private double[] Xi;
+    private double[] Yi;
     private int length;
+    private double xExpectation;
+    private double yExpectation;
+    private double selectionCorrelationsAssignment; //центральный момент 11
+    private double xDispersion;
 
-    public Solution(double[] Xi, double[] Yi){
-        this.Xi=Xi;
-        this.Yi=Yi;
-        length=Xi.length;
+    public Solution(double[] Xi, double[] Yi) {
+        this.Xi = Xi;
+        this.Yi = Yi;
+        length = Xi.length;
     }
 
-    public void estimationOfCorrelationCoefficient(){
+    public void estimationOfCorrelationCoefficient() {
+
+    }
+
+    public void regressionLine() {
+        double bYX = selectionCorrelationsAssignment / Math.sqrt(xDispersion);
+        System.out.printf("b yx = %.3f/%.3f = %.3f", selectionCorrelationsAssignment, Math.sqrt(xDispersion), bYX);
 
     }
 }
