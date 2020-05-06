e1bf55075e795ca43c92d8d4d7f1d9a0e8c12914
Vlada <jagarioendl@gmail.com>
2020-05-06 14:32:47 +0300

    added estimationOfCorrelationCoefficient

--- src/main/java/Solution.java
+++ src/main/java/Solution.java
@@ -17,7 +17,16 @@ public class Solution {
         length = Xi.length;
         xExpectation = 1.0/length * (DoubleStream.of(Xi).sum());
         yExpectation = 1.0/length * (DoubleStream.of(Yi).sum());
-        System.out.println("Its your Expectations for X and Y:");
+
+        selectionCorrelationsAssignment = GetKxy(false);
+        xDispersion = calcDispersion(Xi, xExpectation, false);
+        yDispersion = calcDispersion(Yi, yExpectation, false);
+        r = GetR(false);
+
+
+    }
+    //1
+    public void estimationOfCorrelationCoefficient() {
         StringBuilder XExp = new StringBuilder();
         XExp.append("XExpectation = 1/").append(length).append("(");
         for (double i:this.Xi){
@@ -27,15 +36,29 @@ public class Solution {
         System.out.println(XExp.toString());
         StringBuilder YExp = new StringBuilder();
         YExp.append("YExpectation = 1/").append(length).append("(");
+
         for (double i:this.Yi){
             YExp.append(i).append(" + ");
         }
         YExp.append(") = ").append("1/").append(length).append("*").append(DoubleStream.of(Yi).sum()).append(" = ").append(yExpectation);
         System.out.println(YExp.toString());
-    }
 
-    public void estimationOfCorrelationCoefficient() {
+        GetKxy(true);
 
+        System.out.printf("Sx^2 = 1/%d * (", length);
+        calcDispersion(Xi, xExpectation, true);
+        System.out.printf(" = %.3f \n", xDispersion);
+        xDispersion = Math.sqrt(xDispersion);
+        System.out.printf("Sx = %.2f\n", xDispersion);
+
+
+        System.out.printf("Sy^2 = 1/%d * (", length);
+        calcDispersion(Yi, yExpectation, true);
+        System.out.printf(" = %.3f \n", yDispersion);
+        yDispersion = Math.sqrt(yDispersion);
+        System.out.printf("Sy = %.2f\n", yDispersion);
+
+        GetR(true);
     }
 
     public void  hypothesisAboutSignificanceOfCorrelationCoefficient(double alpha){
@@ -57,4 +80,53 @@ public class Solution {
         double b = yExpectation - bYX * xExpectation;
         System.out.printf("y_x = %.3f * x + %.3f\n", bYX, b);
     }
+
+
+    private double GetR(boolean print)
+    {
+        double R = selectionCorrelationsAssignment / (Math.sqrt(xDispersion) * Math.sqrt(yDispersion));
+        if (print)
+            System.out.printf("r = %.3f / (sqrt(%.3f) * sqrt(%.3f) = %.3f", selectionCorrelationsAssignment, xDispersion, yDispersion, R);
+        return R;
+    }
+
+
+    private double GetKxy(boolean print)
+    {
+        double temp = 0;
+        double Kxy = 0;
+        if (print)
+            System.out.printf("Kxy = 1/%d *(", length);
+        for (int i = 0; i < length; i++) {
+            temp = (Xi[i] - xExpectation) * (Yi[i] - yExpectation);
+            Kxy += temp;
+            if (i + 1 == length && print) {
+                System.out.printf("%.3f) \n", temp);
+            } else if (print)
+                System.out.printf("%.3f + ", temp);
+
+        }
+        Kxy *= (1/ (double)length);
+        return Kxy;
+    }
+
+    private double calcDispersion(double[] row, double Expectation, boolean print)
+    {
+        double result = 0;
+        double temp = 0;
+        for (int i = 0; i < length; i++) {
+            temp = Math.pow(row[i] - Expectation,2);
+            result += temp;
+            if (i + 1 == length && print)
+            {
+                System.out.printf("%.3f) ", temp);
+            } else if (print)
+            {
+                System.out.printf("%.3f +", temp);
+            }
+        }
+        result *= (1/ (double)length);
+        return result;
+    }
+
 }
